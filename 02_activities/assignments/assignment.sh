093a891da1c8746ffa65e1a13482c1a148ab9b16
ian-jinkun <zhaojinkunian@gmail.com>
2025-04-13 12:02:57 -0400

    Revert "Fixed and merged, and finsh the Assignment"
    This reverts commit ef365f72bd7bd395ca3200a6a42207ba467094ca.

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -43,6 +43,7 @@ rm ./data/processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 touch ./data/inventory.txt 
 find ./data/processed > ./data/inventory.txt
+
 # Finished the assignment 
 ###########################################
 
